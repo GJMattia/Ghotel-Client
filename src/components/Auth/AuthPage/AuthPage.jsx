80b269b52eec483af87f33f8a87701844f6b293b
Greg Mattia <gjmattia@gmail.com>
2023-11-29 17:44:20 -0500

    completed style on homepage, sign up component

--- src/components/Auth/AuthPage/AuthPage.jsx
+++ src/components/Auth/AuthPage/AuthPage.jsx
@@ -4,23 +4,40 @@ import LoginForm from '../LoginForm/LoginForm'
 import './AuthPage.css';
 
 export default function AuthPage({ setUser }) {
-  const [showSignUp, setShowSignUp] = useState(false)
+
+  const [signUpDiv, setSignUpDiv] = useState(false);
+
+  function toggleSignUp() {
+    setSignUpDiv(!signUpDiv)
+  }
+
   return (
     <>
 
 
-      {showSignUp ?
-        <SignUpForm setUser={setUser} />
-        :
-        <LoginForm setUser={setUser} />
-      }
+      <LoginForm setUser={setUser} />
 
-      <h1 className='AuthPageTitle'>Habbo Hotel... Bet, Scam, and get angry!</h1>
+
+      {signUpDiv && <SignUpForm setUser={setUser} />}
+      <h1 className='AuthPageTitle'>Ghotel... Build Casinos, Scam, & Profit.</h1>
       <div className='HomePageDiv'>
-        <button className='JoinButton'>Join now for Free</button>
+        <div className='JoinDiv'>
+          <p>New to Ghotel?<span> Click here to</span></p>
+          <button onClick={toggleSignUp} className='JoinButton'>
+            {signUpDiv ? 'Nevermind! STOP!!' : 'Join now for Free'}
+          </button>
+        </div>
       </div>
-      <button className='GoAway' onClick={() => setShowSignUp(!showSignUp)}>{showSignUp ? 'Log In' : 'Sign Up'}</button>
 
+      <ul className='AuthList'>
+        <li>Created by Greg Mattia</li>
+        <li><a className='underline' href='https://docs.google.com/document/d/1N5zmfwLDI_Z3wRA72dflyadcNCkvl4b-/edit?usp=sharing&ouid=115131069856658862221&rtpof=true&sd=true' target="_blank">Resume</a></li>
+        <li><a className='underline' href='https://github.com/GJMattia' target="_blank">Github</a></li>
+        <li><a className='underline' href='https://www.linkedin.com/in/greg-mattia/' target="_blank">LinkedIn</a></li>
+        <li>Powered by React.JS</li>
+      </ul>
+      <p className='AuthDisclaimer'>This is a MERN stack application. A recreation of Habbo Hotel created by Greg Mattia. Made completely from scratch with the exception of borrowing some image assets that I found online.
+        The basis of this project was to recreate a classic game and hone my skills in the software engineering field. No rights reserved. GM - 2023</p>
     </>
   );
 }
--- src/components/Auth/SignUpForm/SignUpForm.jsx
+++ src/components/Auth/SignUpForm/SignUpForm.jsx
@@ -1,3 +1,4 @@
+import './SignUpForm.css';
 import { Component } from 'react';
 import { signUp } from '../../../../utilities/user-services';
 import { createFriendList } from '../../../../utilities/friendlist-api';
@@ -37,22 +38,24 @@ export default class SignUpForm extends Component {
   render() {
     const disable = this.state.password !== this.state.confirm
     return (
-      <div>
-        <div className="form-container">
-          <form autoComplete="off" onSubmit={this.handleSubmit}>
-            <label>Username</label>
-            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
-            <label>Email</label>
-            <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
-            <label>Password</label>
-            <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
-            <label>Confirm</label>
-            <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
-            <button type="submit" disabled={disable}>SIGN UP</button>
-          </form>
-        </div>
-        <p className="error-message">&nbsp;{this.state.error}</p>
+
+      <div className='SignUpFormDiv'>
+        <h2>Enter info to sign up</h2>
+        <form className="SignUpForm" autoComplete="off" onSubmit={this.handleSubmit}>
+          <label>Username</label>
+          <input type="text" name="name" value={this.state.name} onChange={this.handleChange} required />
+          <label>Email</label>
+          <input type="email" name="email" value={this.state.email} onChange={this.handleChange} required />
+          <label>Password</label>
+          <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required />
+          <label>Confirm</label>
+          <input type="password" name="confirm" value={this.state.confirm} onChange={this.handleChange} required />
+          <button className='LoginButton' type="submit" disabled={disable}>SIGN UP</button>
+        </form>
+        <p className="SignUpError">&nbsp;{this.state.error}</p>
       </div>
+
+
     )
   }
 }
