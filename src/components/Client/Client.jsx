c4bd8ed7ef2a51fa85b858e0021ff0e6381513dc
Greg Mattia <gjmattia@gmail.com>
2023-12-05 20:50:44 -0500

    fixed typo again

--- src/components/Client/Client.jsx
+++ src/components/Client/Client.jsx
@@ -19,7 +19,7 @@ export default function Client({ user }) {
 
     return (
         <div className='Client'>
-            <h1>sauce on my boss</h1>
+            <h1>sauce on my friends boss</h1>
             {catalogDiv && <Catalog catalogDiv={catalogDiv} setCatalogDiv={setCatalogDiv} />}
             {consoleDiv && <Console user={user} setChatDiv={setChatDiv} setRoom={setRoom} consoleDiv={consoleDiv} setConsoleDiv={setConsoleDiv} setUsersMessaged={setUsersMessaged} usersMessaged={usersMessaged} />}
             <Chat user={user} chatDiv={chatDiv} setChatDiv={setChatDiv} usersMessaged={usersMessaged} setUsersMessaged={setUsersMessaged} room={room} setRoom={setRoom} />
